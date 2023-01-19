cf064d2b305a8e30107629de23d5590f7a058e21
Charlotte Kies <chopekies@gmail.com>
2023-01-18 20:20:12 -0500

    Re-added backend with .gitignore

---
+++ backend/resource-server/src/main/java/net/codeslate/keycloak/PercussionAssignmentsResourceServerApplication.java
@@ -0,0 +1,12 @@
+package net.codeslate.keycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PercussionAssignmentsResourceServerApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(PercussionAssignmentsResourceServerApplication.class, args);
+	}
+}
