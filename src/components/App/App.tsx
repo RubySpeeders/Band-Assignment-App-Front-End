04bed920201fef7735e3bae52dfc9c40e777eb36
RubySpeeders <sarah@crema.us>
2023-01-27 12:19:14 -0500

    first functioning assignment function and selecting names and instruments

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -20,50 +20,55 @@ export interface Assignment {
 
 export function App() {
   const errorMessage = "People and Istruments not equal"
+
   const [instruments, setInstruments] = useState<Instrument[]>([
     { instrument: "bass drum", selected: false },
     { instrument: "snare", selected: false },
     { instrument: "triangle", selected: false },
   ])
+
   const [names, setNames] = useState<Name[]>([
-    { name: "Chad", selected: true },
+    { name: "Chad", selected: false },
     { name: "Nick", selected: false },
     { name: "Brad", selected: false },
   ])
   const [assignments, setAssignments] = useState<Assignment[]>([])
-  const arraysEqual = instruments.length === names.length
+  const selectedEqual =
+    instruments.filter((instrument) => instrument.selected).length ===
+    names.filter((name) => name.selected).length
 
-  const handleNameSubmit = (values: Name) => {
-    setNames([...names, values])
+  const handleClickName = (item: Name) => {
+    const nextName = names.map((name) => {
+      if (name.name === item.name) {
+        return { name: item.name, selected: !item.selected }
+      } else {
+        return name
+      }
+    })
+    setNames(nextName)
   }
 
-  const handleInstrumentSubmit = (values: Instrument) => {
-    setInstruments([...instruments, values])
+  const handleClickInstrument = (item: Instrument) => {
+    const nextInstrument = instruments.map((instrument) => {
+      if (instrument.instrument === item.instrument) {
+        return { instrument: item.instrument, selected: !item.selected }
+      } else {
+        return instrument
+      }
+    })
+    setInstruments(nextInstrument)
   }
 
-  const handleAssignment = (assignment: Assignment) => {
-    setAssignments([...assignments, assignment])
-  }
-
-  const handleClick = (item: Name | Instrument) => {
-    item.selected = !item.selected
-    console.log(item)
-  }
-
-  const assign = (array1: Name[], array2: Instrument[]) => {
-    const assignment: Assignment = { name: "", instrument: "" }
-    const shuffledNames = shuffle(array1)
-    const shuffledInstruments = shuffle(array2)
-
-    // shuffledNames.forEach((name, i) => {
-    //   assignment.name = name.name
-    //   assignment.instrument = shuffledInstruments[i]
-    //   console.log({ assignment }, i)
-    handleAssignment(assignment)
-    //   console.log(assignments.length)
-    // })
-
-    console.log(shuffledNames, shuffledInstruments, assignment, assignments)
+  const assign = (names: Name[], instruments: Instrument[]) => {
+    // const assignment: Assignment = { name: "", instrument: "" }
+    const shuffledNames = shuffle(names.filter((name) => name.selected))
+    const shuffledInstruments = shuffle(
+      instruments.filter((instrument) => instrument.selected),
+    )
+    const nextAssignment = shuffledNames.map((name, i) => {
+      return { name: name.name, instrument: shuffledInstruments[i].instrument }
+    })
+    setAssignments(nextAssignment)
   }
 
   return (
@@ -71,7 +76,7 @@ export function App() {
       <header className="App-header">
         <h1>Band Assignment</h1>
       </header>
-      {!arraysEqual && <p>{errorMessage}</p>}
+      {!selectedEqual && <p>{errorMessage}</p>}
       <h2>Names</h2>
       {names
         .filter((name) => !name.selected)
@@ -79,7 +84,7 @@ export function App() {
           <Card
             style={{ border: "solid blue", margin: ".25em", width: "50em" }}
             key={name.name}
-            onClick={() => handleClick(name)}
+            onClick={() => handleClickName(name)}
           >
             <CardContent>
               <Typography>{name.name}</Typography>
@@ -94,7 +99,7 @@ export function App() {
             <Card
               style={{ border: "solid blue", margin: ".25em", width: "50em" }}
               key={name.name}
-              onClick={() => handleClick(name)}
+              onClick={() => handleClickName(name)}
             >
               <CardContent>
                 <Typography>{name.name}</Typography>
@@ -102,45 +107,56 @@ export function App() {
             </Card>
           ))}
       </div>
-      <div>
-        <button
-          onClick={() =>
-            handleNameSubmit({ name: "some name", selected: false })
-          }
-        >
-          click me for a good time
-        </button>
-      </div>
+      <div></div>
       <div>
         <h2>Instruments</h2>
-        {instruments.map((instrument) => (
+        {instruments
+          .filter((instrument) => !instrument.selected)
+          .map((instrument) => (
+            <Card
+              style={{ border: "solid red", margin: ".25em", width: "50em" }}
+              key={instrument.instrument}
+              onClick={() => handleClickInstrument(instrument)}
+            >
+              <CardContent>
+                <Typography>{instrument.instrument}</Typography>
+              </CardContent>
+            </Card>
+          ))}
+      </div>
+      <Typography>Selected Instruments</Typography>
+      {instruments
+        .filter((instrument) => instrument.selected)
+        .map((instrument) => (
           <Card
             style={{ border: "solid red", margin: ".25em", width: "50em" }}
             key={instrument.instrument}
-            onClick={() => handleClick(instrument)}
+            onClick={() => handleClickInstrument(instrument)}
           >
             <CardContent>
               <Typography>{instrument.instrument}</Typography>
             </CardContent>
           </Card>
         ))}
-        <button
-          onClick={() =>
-            handleInstrumentSubmit({
-              instrument: "some instrument",
-              selected: false,
-            })
-          }
-        >
-          click me for another good time
-        </button>
-      </div>
       <button
         onClick={() => assign(names, instruments)}
-        disabled={!arraysEqual}
+        disabled={!selectedEqual}
       >
         Give me assignments!
       </button>
+      {assignments.map((assignment) => {
+        return (
+          <Card
+            style={{ border: "solid pink", margin: ".25em", width: "50em" }}
+            key={assignment.name}
+          >
+            <CardContent>
+              <Typography>Name: {assignment.name}</Typography>
+              <Typography>Instrument: {assignment.instrument}</Typography>
+            </CardContent>
+          </Card>
+        )
+      })}
     </div>
   )
 }
